10632a49e5c751c3d2484d808831a7883b81df04
SErgi <sergimartinez16@gmail.com>
2025-02-12 22:47:43 +0100

    segundo

---
+++ Front/frontFestia/src/header/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+function Header () {
+    return (
+        <header>
+            <h1 id="titulo"><a href="">Festia</a></h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Hoy</a></li>
+                    <li><a href="#">Calendario</a></li>
+                </ul>
+            </nav>
+            <a href=""><img src="" alt="imagen-perfil"></img></a>
+            <hr />
+        </header>
+    );
+}
+export default Header;
--- Front/frontFestia/src/main.tsx
+++ Front/frontFestia/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Header from './header/Header.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Header />
   </StrictMode>,
 )
