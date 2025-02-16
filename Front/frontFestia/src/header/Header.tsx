ff5a53b9ca519c701f034d983b8ba4245bf064dc
joan <101748401+joanperezs@users.noreply.github.com>
2025-02-17 00:49:54 +0100

    creación de header
    he instalado tailwind, he puesto los estilos y arreglar bazofias sergi bla bla bla XZDDXD

--- Front/frontFestia/src/header/Header.tsx
+++ Front/frontFestia/src/header/Header.tsx
@@ -1,18 +1,39 @@
-import './Header.css';
+import "./Header.css";
 
-function Header () {
-    return (
-        <header>
-            <h1 id="titulo"><a href="">Festia</a></h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Hoy</a></li>
-                    <li><a href="#">Calendario</a></li>
-                </ul>
-            </nav>
-            <a href=""><img src="" alt="imagen-perfil"></img></a>
-            <hr />
-        </header>
-    );
+function Header() {
+  return (
+    <header className="flex flex-row w-full h-[75px] p-2 border-b-1 border-primary">
+      <div className="flex mx-auto flex-row w-[1270px]">
+        <div className=" basis-[25%] content-center">
+          <h1 id="titulo" className="mr-auto my-auto text-[24px]">
+            Festia
+          </h1>
+        </div>
+        <nav className="flex basis-[50%] ">
+          <ul className="flex w-64 flex-row mx-auto my-auto justify-around">
+            <li>
+              <a href="#" className="hover:underline">
+                Hoy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Calendario
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="basis-[25%] flex">
+          <a className="ml-auto my-auto" href="">
+            <img
+              className="rounded-full w-12"
+              src="https://cdn.prod.website-files.com/62bdc93e9cccfb43e155104c/654f8504462572e0bd680767_Monkey%2520PFP%2520for%2520Tiktok%25207.png"
+              alt=""
+            />
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
 export default Header;
